d450c3418f46f3af24f6364ee8155b038b76cca5
super-rogatory <chukwudimikem@gmail.com>
2024-04-21 01:56:14 -0700

    working on ui, got the main window to pop up

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
 # ENTRY POINT
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from ui.mainwindow import MainWindow
+
+# Personal Notes
+# QApplication holds the Qt event loop, one instance required, application sits in event loop until action is taken ie.) .exec_()
+# similar to the event loop of Node.js w/ async await, events are added to a queue, from queue events go to event handlers and go back to loop awaiting more events
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    app.exec()
+    
+# may or may not need, if executed directly run main(), else if imported use auxiliary function
+if __name__ == "__main__":
+    main()
---
+++ src/ui/mainwindow.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QMainWindow
+# from videoplayer import VideoPlayer
+# from controls import Controls
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Video Matcher") # Window title
+        
+        # set up main layout
+        
+        # initialize video player
+        
+        # initialize controls in the layout
+        
+        # connect controls to video player
